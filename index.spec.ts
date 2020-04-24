ffa0b0275140240f14a946e58ac2b2e4d5811fea
Lucas <lucasvalenteds@gmail.com>
2020-04-23 22:30:12 -0300

    Add example reading, writing and deleting

--- index.spec.ts
+++ index.spec.ts
@@ -1,13 +1,27 @@
 import { StartedTestContainer, GenericContainer } from "testcontainers";
+import Vault from "node-vault";
 
 let container: StartedTestContainer;
+let client: Vault.client;
 
 beforeAll(async (done) => {
-  container = await new GenericContainer("vault", "1.4.0")
+  container = await new GenericContainer("vault", "latest")
     .withEnv("VAULT_DEV_ROOT_TOKEN_ID", "myroot")
     .withEnv("VAULT_DEV_LISTEN_ADDRESS", "0.0.0.0:1234")
+    .withExposedPorts(1234)
     .withNetworkMode("bridge")
     .start();
+
+  const [host, port] = [
+    container.getContainerIpAddress(),
+    container.getMappedPort(1234),
+  ];
+
+  client = Vault({
+    apiVersion: "v1",
+    endpoint: `http://${host}:${port}`,
+    token: "myroot",
+  });
 
   done();
 });
@@ -18,4 +32,40 @@ afterAll(async (done) => {
   done();
 });
 
-test.todo("It can read string");
+beforeEach(async (done) => {
+  await client.write("cubbyhole/dev", {
+    serviceUrl: "http://dev.service.com/api",
+  });
+
+  await client.write("cubbyhole/prod", {
+    serviceUrl: "https://api.service.com",
+    apiToken: "e1d80eef6ddd05ec5fc78583ee9317a0",
+  });
+
+  done();
+});
+
+afterEach(async (done) => {
+  await client.delete("cubbyhole/dev");
+  await client.delete("cubbyhole/prod");
+
+  done();
+});
+
+test("It can read service URL from development environment", async () => {
+  const response = await client.read("cubbyhole/dev");
+
+  expect(response.request_id).toEqual(expect.any(String));
+  expect(response.data.serviceUrl).toStrictEqual("http://dev.service.com/api");
+  expect(response.data.apiToken).toBeUndefined();
+});
+
+test("It can read service URL from production environment", async () => {
+  const response = await client.read("cubbyhole/prod");
+
+  expect(response.request_id).toEqual(expect.any(String));
+  expect(response.data).toStrictEqual({
+    serviceUrl: "https://api.service.com",
+    apiToken: "e1d80eef6ddd05ec5fc78583ee9317a0",
+  });
+});
